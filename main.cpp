f91af7c417fc96277d5760ced8b4a6ddb8bfea62
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2025-01-24 17:18:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,68 @@
-/******************************************************************************
+// This program checks whether a given string is a palindrome using a class structure in C++
+// palindrome is a word, phrase, or sequence of characters that reads the same forward as it does backward
+//  (e.g., "radar", "madam").
 
-                         Let's dive into C++ !
-
-
-*******************************************************************************/
 
 #include <iostream>
+
 using namespace std;
+
+
+
+
+
+class Palindrome{
+    public:
+        // Function declaration
+        bool Check_Palindrome(const std::string& statement);
+
+
+};
+
+
+// Function definition using the scope resolution operator ::
+bool Palindrome::Check_Palindrome(const std::string& statement) {
+    
+    for (int i = 0, j = statement.length() - 1; i < j; i++, j--) {
+        if (statement[i] != statement[j])
+            return false;
+    }
+    return true;
+}
+
+
+
+
+
+int main() {
+
+    std::cout << "Hello, Welcome to the world of magic!" << std::endl;
+    std::cout << "Please give us your string " << std::endl;
+    std::string statement;
+    std::cin >> statement;
+
+    Palindrome object;
+    bool is_palindrome = object.Check_Palindrome(statement);
+
+    if (is_palindrome) {
+        std::cout << "This is a palindrome" << std::endl;
+    } else {
+        std::cout << "This is not a palindrome" << std::endl;
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
 
 
 
