43e981dbe2a27807160188001a7e6be921309bdd
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2025-01-24 17:25:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,74 +1,121 @@
-// This program checks whether a given string is a palindrome using a class structure in C++
-// palindrome is a word, phrase, or sequence of characters that reads the same forward as it does backward
-//  (e.g., "radar", "madam").
+/******************************************************************************
 
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+MCMXCIV
+
+*******************************************************************************/
 
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
 
 
+class romanType{
+
+    str getromannumber(roman);
+    // function to store the roman numeral
+    // return roman numeral
+
+    int convert_roman_to_decimal(roman);
+    // Convert and store the number into decimal form.
+    // retrun decimal form.
+
+    void show_roman(roman);
+
+    void show_decimal();
 
 
-class Palindrome{
-    public:
-        // Function declaration
-        bool Check_Palindrome(const std::string& statement);
+
+};// end of the class
 
 
-};
+
+str romanType::getromannumber(roman){
+    return roman;
+}
 
 
-// Function definition using the scope resolution operator ::
-bool Palindrome::Check_Palindrome(const std::string& statement) {
-    
-    for (int i = 0, j = statement.length() - 1; i < j; i++, j--) {
-        if (statement[i] != statement[j])
-            return false;
+
+int romanType::convert_roman_to_decimal(const string roman){
+
+    // Map to hold Roman numeral values
+    unordered_map<char, int> roman_value = {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000}
+    };
+
+
+    int decimal_value = 0;
+    int n = roman.size();
+
+    for (int i=0; i<n , i++){
+        // If current value is less than next, subtract it, else add it
+        if ( roman_value[roman[i]] < roman_value[roman[i+1]]){
+            decimal_value -=roman_value[roman[i]];
+
+
+        }else{
+
+            decimal_value +=roman_value[roman[i]];
+
+        }
+
     }
-    return true;
+
+    return decimal_value;
+
+
+
+}
+
+
+
+void romanType::show_roman(roman){
+    cout << "The roman numeral is:" << endl << roman;
+
+
+}
+
+void romanType::show_decimal(){
+    cout << "The deciaml is:" << endl << ???;
+
+
+
+
 }
 
 
 
 
 
-int main() {
 
-    std::cout << "Hello, Welcome to the world of magic!" << std::endl;
-    std::cout << "Please give us your string " << std::endl;
-    std::string statement;
-    std::cin >> statement;
 
-    Palindrome object;
-    bool is_palindrome = object.Check_Palindrome(statement);
 
-    if (is_palindrome) {
-        std::cout << "This is a palindrome" << std::endl;
-    } else {
-        std::cout << "This is not a palindrome" << std::endl;
-    }
+
+int main()
+{
+    romanType object;
+
+    cout << "please give us a roman numeral" << endl;
+    string roman;
+    cin>>roman;
+
+    object.getromannumber(roman);
+
+
+
+
 
 
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
