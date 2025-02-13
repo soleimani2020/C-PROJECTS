ef6955d6ab83d80946e09718649ee9627c637d9d
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2025-02-13 16:57:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,59 +1,175 @@
-// This program checks whether a given string is a palindrome using a class structure in C++
-// palindrome is a word, phrase, or sequence of characters that reads the same forward as it does backward
-//  (e.g., "radar", "madam").
+/******************************************************************************
 
+                              OTic tac toe.
+
+
+1. we need a board game (3*3) -> define -> show  
+2. we need two players ( switch player function ): 
+   current player ---> next plaer
+3. we need to know winning condition (check for winner):
+    / Check Raws 
+    / Check columns 
+    / Check diagonals 
+
+4. check if the game is a draw 
+5. If the board has empty spots, then some one has one ! otherwise it is a draw
+
+6. while (invalid move -> n,m >=3) , ask for the right move !  
+
+
+
+*******************************************************************************/
 
 #include <iostream>
-
 using namespace std;
+ 
 
-
-
-
-
-class Palindrome{
-    public:
-        // Function declaration
-        bool Check_Palindrome(const std::string& statement);
-
-
-};
-
-
-// Function definition using the scope resolution operator ::
-bool Palindrome::Check_Palindrome(const std::string& statement) {
+class ticTacToe{
     
-    for (int i = 0, j = statement.length() - 1; i < j; i++, j--) {
-        if (statement[i] != statement[j])
-            return false;
-    }
-    return true;
-}
+    private:
+        char board[3][3];
+        char Current_player;
+
+    public:
+    
+        void Set_Board_game(){
+            for (int i=0; i<3 ; i++){
+                for (int j=0; j<3;j++){
+                    board[i][j]=' ';
+                    }
+                }
+        }
+        
+        
+    
+        void Display_Board_game(){
+            for (int i=0; i<3 ; i++){
+                for (int j=0; j<3;j++){
+                    cout << board[i][j];
+                    if (j<2){
+                        cout << " | "; ;
+                    }
+                }
+                
+            cout << endl;
+            if (i<2){
+                cout << "----------" << endl;
+            }    
+            
+            }
+        }
+        
+        
+        void Player_Switch(){
+            if (Current_player == 'X'){
+                Current_player = 'O';
+            }else{
+                Current_player = 'X';
+            }
+        }
+        
+        
+        int CheckWinningMoves() {
+            // Check Rows and Columns
+            for (int i = 0; i < 3; i++) {
+                if ((board[i][0] == Current_player && board[i][1] == Current_player && board[i][2] == Current_player) ||
+                    (board[0][i] == Current_player && board[1][i] == Current_player && board[2][i] == Current_player)) {
+                    return 1;
+                }
+            }
+        
+            // Check Diagonals
+            if ((board[0][0] == Current_player && board[1][1] == Current_player && board[2][2] == Current_player) ||
+                (board[0][2] == Current_player && board[1][1] == Current_player && board[2][0] == Current_player)) {
+                return 1;
+            }
+        
+            return 0;  // Return 0 if no winning moves
+        }
+
+        
+        
+        int CheckDraw(){
+            for (int i=0; i < 3; i++){
+                for (int j=0; j < 3; j++){
+                    if (board[i][j] == ' '){
+                        return 0;
+                        // sime has one 
+                    }
+  
+                }
+            }
+            
+            return 1;
+            // Draw
+        }
+        
+        void PlayGame(){
+            int n, m;
+            Current_player = 'X';
+            Set_Board_game();  // Initialize the board
+            
+            while(true){
+                Display_Board_game();
+                cout << "Player " << Current_player << " please make a move!" << endl;
+                cout << "Please Enter the row (0-2): " << endl;
+                cin >> m;
+                cout << "Please Enter the column (0-2): " << endl;
+                cin >> n;
+                
+                // Validate the move
+                if (m < 0 || m >= 3 || n < 0 || n >= 3) {
+                    cout << "This is not a valid move! Please enter a row and column between 0 and 2." << endl;
+                    continue;
+                }
+                    
+                
+                if (board[m][n] != ' ') {
+                    cout << "This spot is already taken!" << endl;
+                    continue;
+                }
+                
+                // Place the current player's move on the board
+                board[m][n] = Current_player;
+                
+                // Check if the current player has won
+                if (CheckWinningMoves()) {
+                    Display_Board_game();
+                    cout << "Player " << Current_player << " has won the game! Hooray!" << endl;
+                    break;
+                }
+    
+                // Check for a draw
+                if (CheckDraw()) {
+                    Display_Board_game();
+                    cout << "The game is a draw!" << endl;
+                    break;
+                }
+                
+                // Switch to the other player
+                Player_Switch();
+       
+             }
+        }
+
+}; 
 
 
 
-
-
-int main() {
-
-    std::cout << "Hello, Welcome to the world of magic!" << std::endl;
-    std::cout << "Please give us your string " << std::endl;
-    std::string statement;
-    std::cin >> statement;
-
-    Palindrome object;
-    bool is_palindrome = object.Check_Palindrome(statement);
-
-    if (is_palindrome) {
-        std::cout << "This is a palindrome" << std::endl;
-    } else {
-        std::cout << "This is not a palindrome" << std::endl;
-    }
-
-
-
+int main()
+{
+    ticTacToe game;
+    cout << "Welcome to our little game!" << endl;
+    game.PlayGame();
+    cout << "End of the game"<< endl;
     return 0;
 }
+
+
+
+
+
+
 
 
 
